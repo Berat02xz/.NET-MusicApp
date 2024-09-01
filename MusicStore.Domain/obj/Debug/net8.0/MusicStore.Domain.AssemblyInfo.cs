0362aa7bb8e26336c0abc70ee1ca4e7d0c117c72
BeratAhmetaj <beratahmetaj02@gmail.com>
2024-09-01 23:16:22 +0200

    Artist UI improvement
    +Artist Index basic revamp
    +Artist Details basic card
    
    Checkout stripe logo added
    Tags added to album and artists view
    Tags are separeted by semicolons ;
    
    Co-Authored-By: Aneta Solunska <100601635+Solunska@users.noreply.github.com>

--- MusicStore.Domain/obj/Debug/net8.0/MusicStore.Domain.AssemblyInfo.cs
+++ MusicStore.Domain/obj/Debug/net8.0/MusicStore.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicStore.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68f61bedda90ed298ec131088c97fad5b99386e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e9692acc9207f0643439c605396e231e98e7dd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicStore.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicStore.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
