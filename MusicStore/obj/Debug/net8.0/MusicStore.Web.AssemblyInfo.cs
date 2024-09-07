a8e59c5d83866804f886be28f48050d3bd93a856
Darko Davchev <a_solunska@outlook.com>
2024-09-07 20:45:13 +0200

    changed container width

--- MusicStore/obj/Debug/net8.0/MusicStore.Web.AssemblyInfo.cs
+++ MusicStore/obj/Debug/net8.0/MusicStore.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicStore.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edffd1debd127e987e7680406cd19ae20fddb390")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+022a57d428f045241baa25d174872b186e4a6d69")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicStore.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicStore.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
